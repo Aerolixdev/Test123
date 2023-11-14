af7de71c59b94902583e1ce37a275416f5296455
WyattJP <79605302+WyattJPrice@users.noreply.github.com>
2023-11-14 14:42:50 -0600

    s

--- unblock.js
+++ unblock.js
@@ -1,6 +1,12 @@
 function tube() {
     var address = prompt("Youtube Video URL?");
-   var boolean = address.startsWith("https://www.youtube.com/");
-   alert(boolean);
+    var boolean = address.startsWith("https://www.youtube.com/");
+    if (boolean === true) {
+        alert("is true")
+    } else {
+        alert("its false")
+    }
 }
+
+
 
