08f3896e2beaef0ab5cb4d1ff40c88976d07082f
WyattJP <79605302+WyattJPrice@users.noreply.github.com>
2023-11-15 08:49:35 -0600

    n

--- unblock.js
+++ unblock.js
@@ -2,11 +2,19 @@ function tube() {
     var address = prompt("Youtube Video URL?");
     var boolean = address.startsWith("https://www.youtube.com/");
     if (boolean === true) {
-        alert("is true")
+        extract(address)
     } else {
-        alert("its false")
+        alert("Not a valid youtube URL")
     }
 }
 
+function extract(address) {
+    newString = address.replace("https://www.youtube.com/watch?v=", "");
+    videoid = newString.replace("&authuser=0", "");
+    alert(videoid)
+}
+
+
+/* <iframe width="853" height="480" src="https://www.youtube.com/embed/EtNKG9LUz-w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */
 
 
